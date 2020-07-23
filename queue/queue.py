8da6fed0a39bf1117ba2dbfbb43ff60ab2a6f329
Jeffrey <jeffreymartespolanco@hotmail.com>
2020-07-23 15:55:11 -0400

    Finished queue stretch problem. Had to copy/paste code becauseimporting was impossible

--- queue/queue.py
+++ queue/queue.py
@@ -12,40 +12,83 @@ return elements in First In First Out order.
 
    An array with Queue will have extra memory created even if is not being used due to its Static Memory Allocation at compile time.
    Where a Linked List creates memory at runtime when a new node is inserted.
-   
+
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
 
-class Queue:
-    def __init__(self):
-        self.size = 0
-        self.storage = LinkedList()
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
     
-    def __len__(self):
-        return self.size
+#     def __len__(self):
+#         return self.size
         
 
-    def enqueue(self, value):
+#     def enqueue(self, value):
 
-        self.storage.add_to_tail(value)
-        self.size += 1
+#         self.storage.add_to_tail(value)
+#         self.size += 1
 
-    def dequeue(self):
+#     def dequeue(self):
         
-        if self.storage.length == 0:
-            return None
-        else:
-            self.size -= 1
-            return self.storage.remove_head()
+#         if self.storage.length == 0:
+#             return None
+#         else:
+#             self.size -= 1
+#             return self.storage.remove_head()
             
         
 
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.storage = []
+
+    def __len__(self):
+
+        return self.size
+
+    def push(self, value):
+        self.size += 1
+        return self.storage.append(value)
+
+    def pop(self):
+        if self.size == 0: return 
+        else:
+            self.size -= 1
+            return self.storage.pop()
 
 
 
+class Queue:
+    def __init__(self):
+        self.size = 0
+        self.in_stack = Stack()
+        self.out_stack = Stack()
 
+    def __len__(self):
+        return self.size
+
+    def enqueue(self,value):
+
+        self.in_stack.push(value)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size == 0:return
+
+        self.size -= 1
+        # if out_stack array is empty:
+        # while in_stack is not empty:
+        # push to out_stack the element that is on in_stack
+        # Out of the while and if statement, return the item stored on out_stack array
+        if self.out_stack.size == 0:
+            while self.in_stack.__len__() > 0:
+                self.out_stack.push(self.in_stack.pop())
+        return self.out_stack.storage.pop()
 
 
 
---
+++ stack/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ck), 1)
+        self.stack.push(4)
